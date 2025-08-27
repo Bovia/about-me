95159e41557b85f0af74200748af1eaed4d178e5
Bovia <375006500@qq.com>
2025-08-27 16:01:07 +0800

    移除 .history 文件并更新 .gitignore
    - 从 Git 跟踪中移除所有 .history 文件
    - 更新 .gitignore 忽略编辑器历史文件
    - 防止未来的历史文件被跟踪

--- .history/middleware/redirect.global_20250630142533.ts
+++
@@ -1,6 +0,0 @@
-export default defineNuxtRouteMiddleware((to, from) => {
-  // 将/products路径重定向到/projects
-  if (to.path === '/products') {
-    return navigateTo('/projects', { redirectCode: 301 })
-  }
-}) 
--- .history/server/middleware/redirects_20250630141333.ts
+++
@@ -1,9 +0,0 @@
-export default defineEventHandler((event) => {
-  const url = getRequestURL(event)
-  const path = url.pathname
-
-  // 将 /products 重定向到 /projects
-  if (path === '/products') {
-    return sendRedirect(event, '/projects', 301)
-  }
-}) 
